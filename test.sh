c36c9939970537b592d457cc420c36de85e9097d
Doug Kerwin <dwkerwin@gmail.com>
2018-08-08 16:54:38 -0400

    buildspec testing via script

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Exporting serverless environment variables using hostname $1.forgotpw.com..."
+export REST_API_ID=$( \
+    aws apigateway get-rest-apis \
+      --query 'items[?name==`'"$1"'.forgotpw.com`].[id]' \
+      --output text)
+export ROOT_RESOURCE_ID=$( \
+    aws apigateway get-resources \
+      --rest-api-id $REST_API_ID \
+      --query 'items[?path==`/`].[id]' \
+      --output text)
+export V1_RESOURCE_ID=$( \
+    aws apigateway get-resources \
+      --rest-api-id $REST_API_ID \
+      --query 'items[?path==`/v1`].[id]' \
+      --output text)
+sls invoke test
